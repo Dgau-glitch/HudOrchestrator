b0a959efb0eb7638d9362344d4293e7ded08e73f
Dgau-Glitch <dgau5@mail.ru>
2026-04-06 10:34:00 +0700

    first

---
+++ src/main/kotlin/ru/fatumsoft/hudOrchestrator/HudOrchestrator.kt
@@ -0,0 +1,14 @@
+package ru.fatumsoft.hudOrchestrator
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class HudOrchestrator : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
